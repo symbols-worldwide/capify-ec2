5b5eba8574a604710ea5070def840a68e9c56967
Tim Down <tim@widgit.com>
2021-07-08 12:17:50 +0100

    Another attempt to fix errors resulting from Fog API changes

--- lib/capify-ec2/server.rb
+++ lib/capify-ec2/server.rb
@@ -2,8 +2,8 @@ require 'rubygems'
 require 'fog/aws/models/compute/server'
 
 module Fog
-  module Compute
-    class AWS
+  module AWS
+    class Compute
       class Server
         def contact_point
           private_ip_address || dns_name || public_ip_address
--- lib/capify-ec2/version.rb
+++ lib/capify-ec2/version.rb
@@ -1,6 +1,6 @@
 module Capify
   module Ec2
-    VERSION = "1.8.4"
+    VERSION = "1.8.4.2"
   end
 end
 
